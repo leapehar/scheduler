04dcf374f9ff8a78f5130f32ea3f7a115ef657cd
Lea Pehar <leapehar@gmail.com>
2022-02-26 18:48:52 -0500

    added DayListItem component

---
+++ src/components/DayListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import classNames from "classnames";
+// import "./DayListItem.scss"
+
+export default function DayListItem(props) {
+
+  const numOfSpots = (spots) => {
+    if (spots === 0) {
+      return "no sports remaining"
+    }
+    return `${spots} spots remining`;
+  }
+
+  let dayListItemClass = classNames({
+    "day-list__item": true,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+
+  })
+
+
+
+  return (
+    <li onClick={() => props.setDay(props.name)} >
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{numOfSpots(props.spots)}</h3>
+    </li>
+  );
+}
