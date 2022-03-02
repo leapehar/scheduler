37a58d6d43203d655e6b76626dc9460341c1daf8
Lea Pehar <leapehar@gmail.com>
2022-03-02 07:29:16 -0500

    adding GET request to /api/days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import "components/Application.scss";
 
 import DayList from "./DayList";
@@ -7,23 +7,26 @@ import "components/Application";
 import Appointment from "components/Appointment";
 
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
+
+
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 
 const appointments = [
@@ -68,6 +71,20 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+
+  //axios request here...
+  useEffect(() => {
+    axios.get("/api/days")
+      .then(response => {
+        setDays([...response.data]);
+      })
+
+
+
+
+  }, [])
   return (
     <main className="layout">
       <section className="sidebar">
