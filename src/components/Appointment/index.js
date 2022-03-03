96c277c399a370094a78235c48efa21e80758b39
Lea Pehar <leapehar@gmail.com>
2022-03-03 01:00:37 -0500

    adding mode constants Empty and Show

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,30 +3,39 @@ import "./styles.scss"
 import Header from "./Header"
 import Show from "./Show"
 import Empty from "./Empty"
+import useVisualMode from "hooks/useVisualMode";
 
-
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 
 export default function Appointment(props) {
-  // let appointmentInfo;
-  // if (props.time) {
-  //   appointmentInfo = `Appointment at ${props.time}`;
-  // } else {
-  //   appointmentInfo = "No Appointments";
-  // }
 
+  const {mode, transition, back} = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   // come back later to fix interviewer bug
 
-  let appointmentInfo = props.interview ? <Show student={props.interview.student} /> : <Empty />
+  // let appointmentInfo = props.interview ? <Show student={props.interview.student} /> : <Empty />
 
 
   return (
     <Fragment>
 
       <article className="appointment">
-        <Header time={props.time}> </Header>
-        {appointmentInfo}
+        <Header time={props.time}>
+          {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+          {mode === SHOW && (
+            <Show
+              student={props.interview.student}
+              interviewer={props.interview.interviewer}
+            />
+          )}
+        </Header>
+        {/*appointmentInfo*/}
+
+
 
 
       </article>
